516771bf30a848b6d8923aa730f8a2cb86b2ef28
lleifhei <loganleifheit@gmail.com>
2025-03-11 20:08:39 -0400

    render puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import {puppyList} from './data.js'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppyList)
+  console.log(puppies)
 
   return (
     <>
+    <div>
+      {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+      }
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
